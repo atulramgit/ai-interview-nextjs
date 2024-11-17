89ca8822018771c0ca6addcef79f2c0550b5e879
Atul-ram-3152 <atulram3152@gmail.com>
2024-11-17 20:40:20 +0530

    worked on env file

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,9 +1,11 @@
+
+
 /**@type { import("drizzle-kit").Config } */
 
 export default {
   schema: './utils/schema.js',
   dialect: 'postgresql',
   dbCredentials: {
-    url: 'postgresql://ai-interview-mocker_owner:9KF6tIAWGTcM@ep-rough-pond-a5qns1sq.us-east-2.aws.neon.tech/ai-interview?sslmode=require',
+    url: process.env.NEXT_PUBLIC_DRIZZLE_DB_URL,
   }
 };
